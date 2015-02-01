cd9ba1201cf5b6fa8d31ade90c4241b3fff309c8
BBK-PiJ-2014-14 <skbigmac@hotmail.com>
2015-02-01 23:07:59 +0000

    MulInstruction written
    Test passed

--- SubInstruction.java
+++ SubInstruction.java
@@ -12,7 +12,7 @@ public class SubInstruction extends Instruction{
 	}
 	
 	public SubInstruction(String label, int result, int op1, int op2) {
-		this(label, "add");
+		this(label, "Sub");
 		this.result = result;
 		this.op1 = op1;
 		this.op2 = op2;
